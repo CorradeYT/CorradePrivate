9ef9475b954cdff8fcb437bfd8b248e6a0213cad
Corrade <114356759+CorradeYT@users.noreply.github.com>
2022-12-15 15:42:38 -0500

    Delete README.lua

--- Corrade Private Loadstrings/README.lua
+++
@@ -1,3 +0,0 @@
-# **Corrade-Private-Scripts**
-
-I better not find anyone of you yodashy retards skidding my shit or i will swat your house, rape your whole family, and stab there deads bodies until they don't exist anymore
